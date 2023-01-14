284e56d8a7fdbf93b344befbf2caf907427ae606
BokarevDanila <danila228337228@gmail.com>
2023-01-14 12:34:30 +0300

    Urp, shooting effect

--- Assets/Dev/Scripts/Bullet.cs
+++ Assets/Dev/Scripts/Bullet.cs
@@ -3,16 +3,4 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private float _speed;
-
-    public void Init()
-    {
-
-    }
-
-    private void FixedUpdate()
-    {
-        _rigidbody.velocity = transform.forward * _speed;
-    }
 }
--- Assets/Dev/Scripts/Weapon.cs
+++ Assets/Dev/Scripts/Weapon.cs
@@ -1,15 +1,58 @@
+using System.Collections;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
 	[SerializeField] private Transform _pointToShootFrom;
+	[SerializeField] private Transform _gunEndPoint;
 	[SerializeField] private Transform _sleeveOutPosition;
 	[SerializeField] private Bullet _bullet;
 	[SerializeField] private GameObject _sleeve;
+	[SerializeField] private GameObject _trail;
+	[SerializeField] private Vector3 _sleevePushDirection = new Vector3(1, 1, 0);
+    [SerializeField] private GameObject _dustEffect;
+    [SerializeField] private LayerMask _target;
 
-	public void Shoot()
+    public void Shoot(Vector3 shootPosition, Vector3 shootDirection)
     {
-		Instantiate(_bullet, _pointToShootFrom.position, _pointToShootFrom.transform.rotation).Init();
-		Instantiate(_sleeve, _sleeveOutPosition.position, _pointToShootFrom.transform.rotation);
+        RaycastHit hit;
+
+		if (Physics.Raycast(shootPosition, shootDirection, out hit, Mathf.Infinity, _target.value))
+		{
+			Instantiate(_dustEffect, hit.point, Quaternion.FromToRotation(hit.point, shootPosition));
+        }
+        TrailRenderer trail = Instantiate(_trail, _gunEndPoint.transform.position, _trail.transform.rotation).GetComponent<TrailRenderer>();
+        StartCoroutine(SpawnTrail(trail, hit));
+
+  //      if (Vector3.Distance(_gunEndPoint.transform.position, hit.point) > 1.75f)
+		//{
+		//	TrailRenderer trail = Instantiate(_trail, _gunEndPoint.transform.position, _trail.transform.rotation).GetComponent<TrailRenderer>();
+		//	StartCoroutine(SpawnTrail(trail, hit));
+		//}
+    }
+
+    public void Shoot()
+    {
+		Vector3 shootDirection = (_pointToShootFrom.position - _gunEndPoint.position).normalized;
+
+		Shoot(_pointToShootFrom.transform.position, shootDirection);
+		//Instantiate(_sleeve, _sleeveOutPosition.position, _pointToShootFrom.transform.rotation).GetComponent<Rigidbody>().AddForce(transform.position + _sleevePushDirection * 4, ForceMode.VelocityChange);
+	}
+
+	private IEnumerator SpawnTrail(TrailRenderer trail, RaycastHit hit)
+	{
+		float time = 0;
+
+		Vector3 startPosition = trail.transform.position;
+
+		while (time < 1f)
+		{
+			trail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
+			time += Time.deltaTime / trail.time;
+
+			yield return null;
+		}
+
+		Destroy(trail.gameObject);
 	}
 }
